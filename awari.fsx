de9f3b038396e5da2df7e76f680cbb3bb8f2ef04
matethemouse <44768694+matethemouse@users.noreply.github.com>
2018-11-05 10:54:52 +0100

    POP Hold 7 Uge 8

---
+++ awari.fsx
@@ -0,0 +1,9 @@
+
+let o_pit=[|3;3;3;3;3;3|]
+let y_pit=[|3;3;3;3;3;3|]
+let o_hom=0
+let y_hom=0
+
+let board=sprintf "\n    %A\n %A                        %A\n    %A\n" y_pit o_hom y_hom o_pit;;
+printfn "%A" boardkgdfjkgjdkl
+printfn "%A" board
